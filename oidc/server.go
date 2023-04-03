5ef0a9b9eae3dae4c315b9f0f7041a319fe5b9d8
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2023-04-03 13:11:21 +0200

    Fix Service Account Response parsing

--- oidc/server.go
+++ oidc/server.go
@@ -101,29 +101,45 @@ func startAndListenHttpServer(channel chan common.OidcCredentialsResponse) {
 	}
 }
 
+func createServiceAccountAuthenticateRequest(requestUrl string, clientId string, clientSecret string) *http.Request {
+	data := url.Values{}
+	data.Set("grant_type", "client_credentials")
+	data.Set("scope", "openid")
+	request := common.GetRequest(http.MethodPost, requestUrl, strings.NewReader(data.Encode()))
+	request.SetBasicAuth(clientId, clientSecret)
+	request.Header.Add(headers.ContentType, "application/x-www-form-urlencoded")
+	return request
+}
+
+type ServiceAccountResponse struct {
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	TokenType        string `json:"token_type"`
+	IdToken          string `json:"id_token"`
+	NotBeforePolicy  int    `json:"not-before-policy"`
+	SessionState     string `json:"session_state"`
+	AccessToken      string `json:"access_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	Scope            string `json:"scope"`
+}
+
 func authenticateServiceAccountWithIdp(params common.AuthInfo) common.OidcCredentialsResponse {
 	idpTokenUrl, err := url.JoinPath(params.IdpUrl, "protocol/openid-connect/token")
 	if err != nil {
 		common.OutputErrorToConsoleAndExit(err)
 	}
-
-	data := url.Values{}
-	data.Set("grant_type", "client_credentials")
-	data.Set("scope", "openid")
-	request := common.GetRequest(http.MethodPost, idpTokenUrl, strings.NewReader(data.Encode()))
-	request.SetBasicAuth(params.ClientId, params.ClientSecret)
-	request.Header.Add(headers.ContentType, "application/x-www-form-urlencoded")
+	request := createServiceAccountAuthenticateRequest(idpTokenUrl, params.ClientId, params.ClientSecret)
 	response := common.HttpClientMakeRequest(request)
 	bodyBytes := common.GetBodyBytesFromResponse(response)
 
-	var values map[string]string
-	err = json.Unmarshal(bodyBytes, &values)
+	var result ServiceAccountResponse
+	err = json.Unmarshal(bodyBytes, &result)
 	if err != nil {
 		common.OutputErrorToConsoleAndExit(err)
 	}
 
 	return common.OidcCredentialsResponse{
-		BearerToken: values["id_token"],
+		BearerToken: result.IdToken,
 	}
 }
 
