10d91cb12bc69cb3d74281216da2e71bc5b0e256
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2023-04-03 13:39:34 +0200

    Refactor Service Account Code

--- oidc/server.go
+++ oidc/server.go
@@ -2,7 +2,6 @@ package oidc
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/go-http-utils/headers"
@@ -10,7 +9,6 @@ import (
 	"github.com/pkg/browser"
 	"golang.org/x/oauth2"
 	"net/http"
-	"net/url"
 	"otc-auth/common"
 	"strings"
 )
@@ -98,48 +96,6 @@ func startAndListenHttpServer(channel chan common.OidcCredentialsResponse) {
 	err := http.ListenAndServe(localhost, nil)
 	if err != nil {
 		common.OutputErrorToConsoleAndExit(err, fmt.Sprintf("failed to start server at %s", localhost))
-	}
-}
-
-func createServiceAccountAuthenticateRequest(requestUrl string, clientId string, clientSecret string) *http.Request {
-	data := url.Values{}
-	data.Set("grant_type", "client_credentials")
-	data.Set("scope", "openid")
-	request := common.GetRequest(http.MethodPost, requestUrl, strings.NewReader(data.Encode()))
-	request.SetBasicAuth(clientId, clientSecret)
-	request.Header.Add(headers.ContentType, "application/x-www-form-urlencoded")
-	return request
-}
-
-type ServiceAccountResponse struct {
-	RefreshExpiresIn int    `json:"refresh_expires_in"`
-	TokenType        string `json:"token_type"`
-	IdToken          string `json:"id_token"`
-	NotBeforePolicy  int    `json:"not-before-policy"`
-	SessionState     string `json:"session_state"`
-	AccessToken      string `json:"access_token"`
-	ExpiresIn        int    `json:"expires_in"`
-	RefreshToken     string `json:"refresh_token"`
-	Scope            string `json:"scope"`
-}
-
-func authenticateServiceAccountWithIdp(params common.AuthInfo) common.OidcCredentialsResponse {
-	idpTokenUrl, err := url.JoinPath(params.IdpUrl, "protocol/openid-connect/token")
-	if err != nil {
-		common.OutputErrorToConsoleAndExit(err)
-	}
-	request := createServiceAccountAuthenticateRequest(idpTokenUrl, params.ClientId, params.ClientSecret)
-	response := common.HttpClientMakeRequest(request)
-	bodyBytes := common.GetBodyBytesFromResponse(response)
-
-	var result ServiceAccountResponse
-	err = json.Unmarshal(bodyBytes, &result)
-	if err != nil {
-		common.OutputErrorToConsoleAndExit(err)
-	}
-
-	return common.OidcCredentialsResponse{
-		BearerToken: result.IdToken,
 	}
 }
 
