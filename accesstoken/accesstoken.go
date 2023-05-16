9d8a95b36818bc23a3115b2fa95f62d774cb89d1
Mweya Ruider <mweya.ruider@iits-consulting.de>
2023-05-16 11:17:39 +0200

    [WIP] Clean up, add second command to use GTC to get AK/SK

--- accesstoken/accesstoken.go
+++ accesstoken/accesstoken.go
@@ -3,6 +3,9 @@ package accesstoken
 import (
 	"fmt"
 	"github.com/go-http-utils/headers"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/credentials"
 	"net/http"
 	"otc-auth/common"
 	"otc-auth/common/endpoints"
@@ -13,7 +16,33 @@ import (
 	"strings"
 )
 
-func CreateAccessToken(durationSeconds int) {
+func CreateAccessToken(durationSeconds int, args ...bool) {
+	if len(args) > 0 {
+		// Use GTC?
+		if args[0] {
+			println("Creating access token file with GTC...")
+			resp, err := getAccessTokenFromServiceProviderGTC(durationSeconds)
+			if err != nil {
+				println("[!] ", err)
+				return
+			}
+
+			accessKeyFileContent := fmt.Sprintf(
+				"export OS_ACCESS_KEY=%s\n"+
+					"export AWS_ACCESS_KEY_ID=%s\n"+
+					"export OS_SECRET_KEY=%s\n"+
+					"export AWS_SECRET_ACCESS_KEY=%s",
+				resp.AccessKey,
+				resp.AccessKey,
+				resp.SecretKey,
+				resp.SecretKey)
+
+			common.WriteStringToFile("./ak-sk-env.sh", accessKeyFileContent)
+			println("Access token file created successfully.")
+			println("Please source the ak-sk-env.sh file in the current directory manually")
+			return
+		}
+	}
 	println("Creating access token file...")
 
 	response := getAccessTokenFromServiceProvider(strconv.Itoa(durationSeconds))
@@ -35,6 +64,27 @@ func CreateAccessToken(durationSeconds int) {
 
 	println("Access token file created successfully.")
 	println("Please source the ak-sk-env.sh file in the current directory manually")
+}
+
+func getAccessTokenFromServiceProviderGTC(durationSeconds int) (*credentials.TemporaryCredential, error) {
+	provider, err := openstack.AuthenticatedClient(golangsdk.AuthOptions{
+		IdentityEndpoint: endpoints.BaseUrlIam + "/v3",
+		DomainID:         config.GetActiveCloudConfig().Domain.Id,
+		TenantID:         config.GetActiveCloudConfig().Domain.Name,
+		TokenID:          config.GetActiveCloudConfig().UnscopedToken.Secret,
+	})
+	if err != nil {
+		return nil, err
+	}
+	client, err := openstack.NewIdentityV3(provider, golangsdk.EndpointOpts{})
+	if err != nil {
+		return nil, err
+	}
+	return credentials.CreateTemporary(client, credentials.CreateTemporaryOpts{
+		Methods:  []string{"token"},
+		Token:    config.GetActiveCloudConfig().UnscopedToken.Secret,
+		Duration: durationSeconds,
+	}).Extract()
 }
 
 func getAccessTokenFromServiceProvider(durationSeconds string) *http.Response {
--- login.go
+++ login.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/opentelekomcloud/gophertelekomcloud"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/credentials"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/projects"
 	"otc-auth/common"
 	"otc-auth/config"
 	"otc-auth/iam"
@@ -23,7 +18,6 @@ func AuthenticateAndGetUnscopedToken(authInfo common.AuthInfo) {
 
 	println("Retrieving unscoped token for active cloud...")
 
-	// TODO remove
 	var tokenResponse common.TokenResponse
 	switch authInfo.AuthType {
 	case "idp":
@@ -31,20 +25,6 @@ func AuthenticateAndGetUnscopedToken(authInfo common.AuthInfo) {
 			tokenResponse = saml.AuthenticateAndGetUnscopedToken(authInfo)
 		} else if authInfo.AuthProtocol == protocolOIDC {
 			tokenResponse = oidc.AuthenticateAndGetUnscopedToken(authInfo)
-			fmt.Println("[*] ", tokenResponse)
-			opts := golangsdk.AuthOptions{
-				IdentityEndpoint: "https://iam.eu-de.otc.t-systems.com:443/v3",
-				DomainID:         "OTC-EU-DE-00000000001000055571",
-				TenantID:         "d32336fe26ec415caa04e17e9adfb6a8",
-				TokenID:          tokenResponse.Token.Secret,
-			}
-			client, err := openstack.AuthenticatedClient(opts)
-			fmt.Println(client, err)
-
-			listProjects(client)
-			// TODO remove
-			//tokenResponse2 = gophertelekomcloud2.TestLogin(authInfo)
-			// fmt.Println(tokenResponse, tokenResponse2, tokenResponse == tokenResponse2)
 		} else {
 			common.OutputErrorMessageToConsoleAndExit("fatal: unsupported login protocol.\n\nAllowed values are \"saml\" or \"oidc\". Please provide a valid argument and try again.")
 		}
@@ -61,100 +41,6 @@ func AuthenticateAndGetUnscopedToken(authInfo common.AuthInfo) {
 	updateOTCInfoFile(tokenResponse)
 	createScopedTokenForEveryProject()
 	println("Successfully obtained unscoped token!")
-}
-
-type ProjectShortDescription struct {
-	description string
-	domainId    string
-	enabled     bool
-	id          string
-	isDomain    bool
-	links       []ProjectLink
-	name        string
-	parentId    string
-}
-
-type ProjectLink struct {
-	description string
-	link        string
-}
-
-func listProjects(provider *golangsdk.ProviderClient) ([]projects.Project, error) {
-	client, err := openstack.NewIdentityV3(provider, golangsdk.EndpointOpts{})
-	if err != nil {
-		return nil, err
-	}
-
-	pager, err := projects.List(client, projects.ListOpts{}).AllPages()
-	var projectList []projects.Project
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	prts, err := pager.GetBodyAsMap()
-
-	prtsI := prts["projects"]
-
-	prtsS := prtsI.([]interface{})
-
-	var arr []ProjectShortDescription
-
-	for s := range prtsS {
-		proj := prtsS[s].(map[string]interface{})
-		projS := ProjectShortDescription{}
-		for i, j := range proj {
-			switch i {
-			case "description":
-				projS.description = j.(string)
-			case "domain_id":
-				projS.domainId = j.(string)
-			case "enabled":
-				projS.enabled = j.(bool)
-			case "id":
-				projS.id = j.(string)
-			case "is_domain":
-				projS.isDomain = j.(bool)
-			case "links":
-				var arr2 []ProjectLink
-				for u := range j.(map[string]interface{}) {
-					val := j.(map[string]interface{})[u]
-					if val != nil {
-						k := ProjectLink{}
-						k.description = u
-						k.link = val.(string)
-						arr2 = append(arr2, k)
-					}
-				}
-				projS.links = arr2
-			case "name":
-				projS.name = j.(string)
-			}
-		}
-		arr = append(arr, projS)
-	}
-
-	fmt.Println("aaa", arr)
-
-	// Try to get AK / SK
-
-	creds, err := credentials.CreateTemporary(client, credentials.CreateTemporaryOpts{
-		Methods:  []string{"token"},
-		Token:    client.Token(),
-		Duration: 60,
-	}).Extract()
-
-	fmt.Println(creds)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return projectList, nil
 }
 
 func createScopedTokenForEveryProject() {
