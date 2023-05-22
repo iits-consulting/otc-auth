9818074284cd6954ba0aa14ab29fb52553f6f17c
Mweya Ruider <mweya.ruider@iits-consulting.de>
2023-05-22 12:14:20 +0200

    [WIP] Get projects using lib

--- iam/projects.go
+++ iam/projects.go
@@ -1,29 +1,29 @@
 package iam
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/go-http-utils/headers"
-	"net/http"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/projects"
 	"otc-auth/common"
 	"otc-auth/common/endpoints"
-	"otc-auth/common/headervalues"
-	"otc-auth/common/xheaders"
 	"otc-auth/config"
 	"strings"
 )
 
 func GetProjectsInActiveCloud() config.Projects {
 	projectsResponse := getProjectsFromServiceProvider()
-	var projects config.Projects
+	var cloudProjects config.Projects
 	for _, project := range projectsResponse.Projects {
-		projects = append(projects, config.Project{
+		cloudProjects = append(cloudProjects, config.Project{
 			NameAndIdResource: config.NameAndIdResource{Name: project.Name, Id: project.Id},
 		})
 	}
 
-	config.UpdateProjects(projects)
-	println(fmt.Sprintf("Projects for active cloud:\n%s", strings.Join(projects.GetProjectNames(), ",\n")))
-	return projects
+	config.UpdateProjects(cloudProjects)
+	println(fmt.Sprintf("Projects for active cloud:\n%s", strings.Join(cloudProjects.GetProjectNames(), ",\n")))
+	return cloudProjects
 }
 
 func CreateScopedTokenForEveryProject(projectNames []string) {
@@ -32,17 +32,32 @@ func CreateScopedTokenForEveryProject(projectNames []string) {
 	}
 }
 
-func getProjectsFromServiceProvider() (projectsResponse common.ProjectsResponse) {
+func getProjectsFromServiceProvider() common.ProjectsResponse {
 	cloud := config.GetActiveCloudConfig()
 	println(fmt.Sprintf("info: fetching projects for cloud %s", cloud.Domain.Name))
 
-	request := common.GetRequest(http.MethodGet, endpoints.IamProjects, nil)
-	request.Header.Add(headers.ContentType, headervalues.ApplicationJson)
-	request.Header.Add(xheaders.XAuthToken, cloud.UnscopedToken.Secret)
+	provider, err := openstack.AuthenticatedClient(golangsdk.AuthOptions{
+		IdentityEndpoint: endpoints.BaseUrlIam + "/v3",
+		DomainID:         config.GetActiveCloudConfig().Domain.Id,
+		TokenID:          cloud.UnscopedToken.Secret,
+	})
+	if err != nil {
+		common.OutputErrorToConsoleAndExit(err)
+	}
+	client, err := openstack.NewIdentityV3(provider, golangsdk.EndpointOpts{})
 
-	response := common.HttpClientMakeRequest(request)
-	bodyBytes := common.GetBodyBytesFromResponse(response)
-	projectsResponse = *common.DeserializeJsonForType[common.ProjectsResponse](bodyBytes)
+	projectsResponse, err := projects.List(client, projects.ListOpts{}).AllPages()
+	if err != nil {
+		common.OutputErrorToConsoleAndExit(err)
+	}
 
-	return projectsResponse
+	projectsResponseMap := projectsResponse.GetBody()
+	// forgive me
+	var out common.ProjectsResponse
+	err = json.Unmarshal(projectsResponseMap, &out)
+	if err != nil {
+		common.OutputErrorToConsoleAndExit(err)
+	}
+
+	return out
 }
