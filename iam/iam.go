2d675c9a49672fbe8e7856ba44275f7b0a2a46f5
Mweya Ruider <mweya.ruider@iits-consulting.de>
2023-05-24 11:33:39 +0200

    [WIP] IAM scoped tokens via gotc

--- iam/iam.go
+++ iam/iam.go
@@ -3,19 +3,12 @@ package iam
 import (
 	"errors"
 	"fmt"
-	"github.com/avast/retry-go"
-	"github.com/go-http-utils/headers"
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/tokens"
-	"log"
-	"net/http"
 	"otc-auth/common"
 	"otc-auth/common/endpoints"
-	"otc-auth/common/headervalues"
-	"otc-auth/common/xheaders"
 	"otc-auth/config"
-	"strings"
 	"time"
 )
 
@@ -73,7 +66,7 @@ func GetScopedToken(projectName string) config.Token {
 	}
 
 	println("attempting to request a scoped token.")
-	getScopedTokenFromServiceProvider(projectName)
+	getScopedTokenFromServiceProvider(projectName) // TODO - Seems a little dirty, might want to actually return a value and not have the cloud config updated as a side-effect
 	project = config.GetActiveCloudConfig().Projects.GetProjectByNameOrThrow(projectName)
 	return project.ScopedToken
 }
@@ -82,53 +75,40 @@ func getScopedTokenFromServiceProvider(projectName string) {
 	cloud := config.GetActiveCloudConfig()
 	projectId := cloud.Projects.GetProjectByNameOrThrow(projectName).Id
 
-	err := retry.Do(
-		func() error {
-			requestBody := fmt.Sprintf("{\"auth\": {\"identity\": {\"methods\": [\"token\"], \"token\": {\"id\": \"%s\"}}, \"scope\": {\"project\": {\"id\": \"%s\"}}}}", cloud.UnscopedToken.Secret, projectId)
+	authOpts := golangsdk.AuthOptions{
+		IdentityEndpoint: endpoints.BaseUrlIam + "/v3",
+		TokenID:          cloud.UnscopedToken.Secret,
+		TenantID:         projectId,
+		DomainName:       cloud.Domain.Name,
+	}
 
-			request := common.GetRequest(http.MethodPost, endpoints.IamTokens, strings.NewReader(requestBody))
-			request.Header.Add(headers.ContentType, headervalues.ApplicationJson)
-
-			response := common.HttpClientMakeRequest(request)
-
-			scopedToken := response.Header.Get(xheaders.XSubjectToken)
-
-			if scopedToken == "" {
-				bodyBytes := common.GetBodyBytesFromResponse(response)
-				formattedError := common.ByteSliceToIndentedJsonFormat(bodyBytes)
-				defer response.Body.Close()
-				println("error: an error occurred while polling a scoped token. Will try again")
-				return fmt.Errorf("http status code: %s\nresponse body:\n%s", response.Status, formattedError)
-			}
-
-			bodyBytes := common.GetBodyBytesFromResponse(response)
-			tokenResponse := common.DeserializeJsonForType[common.TokenResponse](bodyBytes)
-			defer response.Body.Close()
-
-			token := config.Token{
-				Secret:    scopedToken,
-				IssuedAt:  tokenResponse.Token.IssuedAt,
-				ExpiresAt: tokenResponse.Token.ExpiresAt,
-			}
-			index := cloud.Projects.FindProjectIndexByName(projectName)
-			if index == nil {
-				errorMessage := fmt.Sprintf("fatal: project with name %s not found.\n\nUse the cce list-projects command to get a list of projects.", projectName)
-				common.OutputErrorToConsoleAndExit(errors.New(errorMessage))
-			}
-			cloud.Projects[*index].ScopedToken = token
-			config.UpdateCloudConfig(cloud)
-			println("scoped token acquired successfully.")
-
-			return nil
-		}, retry.OnRetry(func(n uint, err error) {
-			log.Printf("#%d: %s\n", n, err)
-		}),
-		retry.DelayType(retry.FixedDelay),
-		retry.Delay(time.Second*5),
-	)
+	provider, err := openstack.AuthenticatedClient(authOpts)
 	if err != nil {
 		common.OutputErrorToConsoleAndExit(err)
 	}
+	client, err := openstack.NewIdentityV3(provider, golangsdk.EndpointOpts{})
+	if err != nil {
+		common.OutputErrorToConsoleAndExit(err)
+	}
+
+	scopedToken, err := tokens.Create(client, &authOpts).ExtractToken()
+	if err != nil {
+		common.OutputErrorToConsoleAndExit(err)
+	}
+
+	token := config.Token{
+		Secret: scopedToken.ID,
+		// TODO IssuedAt:  tokenResponse.Token.IssuedAt,
+		ExpiresAt: scopedToken.ExpiresAt.Format(time.RFC3339),
+	}
+	index := cloud.Projects.FindProjectIndexByName(projectName)
+	if index == nil {
+		errorMessage := fmt.Sprintf("fatal: project with name %s not found.\n\nUse the cce list-projects command to get a list of projects.", projectName)
+		common.OutputErrorToConsoleAndExit(errors.New(errorMessage))
+	}
+	cloud.Projects[*index].ScopedToken = token
+	config.UpdateCloudConfig(cloud)
+	println("scoped token acquired successfully.")
 }
 
 func getRequestBodyForAuthenticationMethod(authInfo common.AuthInfo) (requestBody string) {
