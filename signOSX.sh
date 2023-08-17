69b84ba71a2a57c1abd52fc7d9850a9f2a1b2d9f
Mweya Ruider <mweya.ruider@iits-consulting.de>
2023-08-17 16:00:11 +0200

    Notarize binary on OSX too

--- signOSX.sh
+++ signOSX.sh
@@ -3,10 +3,21 @@ set -euo pipefail
 
 binary_path=$1
 binary_os=$2
+binary_version=$3
+binary_arch=$4
 
 exec >> post_build_output.txt 2>&1
 
 if [ "$binary_os" == "darwin" ]; then
-  codesign --keychain build.keychain --sign "Mac Developer: Mweya Ruider (ZHBMW6QG35)" $binary_path
-  codesign --verify -vvvv $binary_path
+  codesign --keychain build.keychain --sign "Developer ID Application: Impresssol GmbH (J486APST94)" --options=runtime --timestamp --deep $binary_path
+  codesign -vvvv --deep --strict $binary_path
+  codesign -dvvv --entitlements :- $binary_path
+  echo "[*] Binary signed"
+  zip "$binary_path.zip" $binary_path
+  xcrun notarytool submit "$binary_path.zip" \
+               --apple-id "mweya.ruider@iits-consulting.de" \
+               --password $NOTARYTOOL_PASS \
+               --team-id "J486APST94" \
+               --wait
+  echo "[*] Binary submitted"
 fi
