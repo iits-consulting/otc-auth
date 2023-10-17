d442cd5642cf9694f1f5e7552cd3a7f3fd185663
Mweya Ruider <mweya.ruider@iits-consulting.de>
2023-10-17 12:41:01 +0200

    fix(errors): Don't exit as normal if error happens

--- common/http_client.go
+++ common/http_client.go
@@ -36,24 +36,31 @@ func GetRequest(method string, url string, body io.Reader) *http.Request {
 	return request
 }
 
+func closeStreamCheckErr(body io.ReadCloser, err error) {
+	errBodyClose := body.Close()
+	if errBodyClose != nil {
+		err = fmt.Errorf("fatal: %w\nfatal: error closing response body\n%w", err, errBodyClose)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func GetBodyBytesFromResponse(response *http.Response) []byte {
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Fatalf("fatal: error closing response body.\ntrace: %s", err)
-		}
-	}(response.Body)
+	var err error
 
 	bodyBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Printf("fatal: error reading response body.\ntrace: %s", err)
+		err = fmt.Errorf("fatal: error reading response body\n%w", err)
+		closeStreamCheckErr(response.Body, err)
 	}
 
 	statusCodeStartsWith2 := regexp.MustCompile(`2\d{2}`)
 	if !statusCodeStartsWith2.MatchString(strconv.Itoa(response.StatusCode)) {
-		errorMessage := fmt.Sprintf("error: status %s, body:\n%s", response.Status, bodyBytes)
-		log.Print(errorMessage)
+		err = fmt.Errorf("fatal: status %s, body:\n%s", response.Status, bodyBytes)
+		closeStreamCheckErr(response.Body, err)
 	}
 
+	defer closeStreamCheckErr(response.Body, err)
 	return bodyBytes
 }
