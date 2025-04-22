a30b1367c4d351114063755eadb5ef88ec15d756
Mweya <mweya.ruider@iits-consulting.de>
2025-04-22 11:51:39 +0200

    chore(ci): Fail if step fails

--- format_and_lint.sh
+++ format_and_lint.sh
@@ -3,11 +3,11 @@
 PATH="$PATH:$HOME/go/bin"
 
 reset
-go mod tidy
-go test -v ./...
-gci write .
-go vet .
-goimports -w .
-gofmt -w .
-gofumpt -w .
+go mod tidy           && \
+go test -v ./...      && \
+gci write .           && \
+go vet .              && \
+goimports -w .        && \
+gofmt -w .            && \
+gofumpt -w .          && \
 golangci-lint run -v
